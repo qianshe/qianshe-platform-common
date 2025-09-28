9acbc7d579d31a997c933e24b991dfa1fcb7ea62
qianshe <cutoffother@gmail.com>
2025-09-28 16:36:00 +0800

    fix(notification): 修改数据库连接URL中的数据库名
    将application.yml配置文件中的数据库名从qianshe_notification更正为notification，
    以匹配实际的数据库实例名称。

--- file-storage/src/main/java/com/qianshe/filestorage/entity/FileInfo.java
+++ file-storage/src/main/java/com/qianshe/filestorage/entity/FileInfo.java
@@ -1,13 +1,10 @@
 package com.qianshe.filestorage.entity;
 
+import com.baomidou.mybatisplus.annotation.*;
 import com.qianshe.filestorage.enums.FileAccessType;
 import com.qianshe.filestorage.enums.FileStatus;
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
